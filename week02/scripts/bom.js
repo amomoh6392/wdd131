48f709a7dcd4c40e971aa482b3f0faab1f7a81f1
amomoh6392 <amomoh@byupathway.edu>
2026-05-13 04:31:55 +0100

    learning activity

--- week02/scripts/bom.js
+++ week02/scripts/bom.js
@@ -1,9 +1,41 @@
 const input = document.querySelector("#favchap");
 const button = document.querySelector("button");
-const list = document.querySelector("____________");
-const li = document.createElement("li");
-const deleteButton = document.createElement("button");
-li.textContent = input.Value;
-deleteButton.textContent = "❌";
-li.append(deleteButton);
-list.append(li);
+const list = document.querySelector("#list");
+
+button.addEventListener('click', function () {
+    const chapter = input.value.trim();
+    if (chapter !== "") {
+        const items = list.querySelectorAll("li");
+
+        let exists = false;
+        items.forEach(function (item) {
+            const text = item.firstChild.textContent;
+
+            if (text === chapter) {
+                exists = true;
+            }
+        });
+
+       
+        if (!exists) {
+            const li = document.createElement("li");
+            li.textContent = input.value;
+            
+            const deleteButton = document.createElement("button");
+            deleteButton.textContent = "❌";
+
+            li.appendChild(deleteButton);
+            li.appendChild(deleteButton);
+            list.appendChild(li);
+            deleteButton.addEventListener("click", function () {
+                list.removeChild(li);
+                input.focus();
+            });
+            
+        } else {
+            alert("This chapter already exists!");
+        }
+        input.value = "";
+    }
+    input.focus();
+});
