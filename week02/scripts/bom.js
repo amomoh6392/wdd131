43c48df95c6375a678153074e3dd78c4d7cf6adc
amomoh6392 <amomoh@byupathway.edu>
2026-05-13 03:48:14 +0100

    learning activity

---
+++ week02/scripts/bom.js
@@ -0,0 +1,9 @@
+const input = document.querySelector("#favchap");
+const button = document.querySelector("button");
+const list = document.querySelector("____________");
+const li = document.createElement("li");
+const deleteButton = document.createElement("button");
+li.textContent = input.Value;
+deleteButton.textContent = "❌";
+li.append(deleteButton);
+list.append(li);
