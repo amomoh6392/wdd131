86311945a4654f9ebfd9ec9b75509b882442a81f
amomoh6392 <amomoh@byupathway.edu>
2026-05-14 23:22:34 +0100

    update on assignment page

--- scripts/temples.js
+++ scripts/temples.js
@@ -3,11 +3,11 @@ document.getElementById("currentyear").textContent = new Date().getFullYear();
 document.getElementById("lastmodified").textContent = `Last Modified: ${document.lastModified}`;
 
 const menuButton = document.querySelector("#menu");
-const navigation = document.querySelector(".navigation");
+const navigation = document.querySelector("nav");
 menuButton.addEventListener("click", () => {
     navigation.classList.toggle("open");
     menuButton.classList.toggle("open");
-    
+
     const isOpen = navigation.classList.contains("open");
     menuButton.setAttribute("aria-expanded", isOpen);
 });
